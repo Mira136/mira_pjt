de21978666dcce7ccc666014fc768627f74b4970
Mira136 <mirap7365@gmail.com>
2025-07-17 19:21:42 +0530

    Pattern Programs

---
+++ Pattern6.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace mira_pjt
+{
+    class Pattern6
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (j == 0 || j == 2 || i == 0 || i == n)
+                    {
+                        Console.Write(n + " ");
+                    }
+                    else
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+/* 
+3 3 3
+3 1 3 
+3 2 3
+3 3 3
+
+4 4 4
+4 1 4
+4 2 4
+4 3 4
+4 4 4 
+*/
---
+++ Pattern7.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace mira_pjt
+{
+    class Pattern7
+    {
+        static void Main(string[] args)
+        {
+            int rows = 4;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                        Console.Write("* ");
+                    else
+                        Console.Write("# ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+/*
+*
+# *
+* # *
+# * # *
+*/
+
