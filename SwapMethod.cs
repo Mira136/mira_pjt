9ee11679d98f74fbf6acab2611953d98b1aa03d6
Mira136 <mirap7365@gmail.com>
2025-07-24 11:07:03 +0530

    commit

---
+++ SwapMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace mira_pjt
+{
+    class SwapMethod
+    {
+        static void Swap(ref int a, ref int b) // a and b both are local variable
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            Console.WriteLine("Before swap : a = {0} and b = {1}",  a, b);
+           // Swap(a, b); // passing the argument by value
+            Swap(ref a, ref b); // passing the argument by reference
+            Console.WriteLine("After swap : a = {0} and b = {1}", a, b);
+        }
+    }
+}
+
